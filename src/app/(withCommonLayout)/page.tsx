b7c2be54de05bddccf7a4a6628bb5b8b5f1646ab
rezwan2230 <rezwan2230@gmail.com>
2025-05-05 23:26:36 +0600

    added group route and create a layout for this and also create a register route in app directory

---
+++ src/app/(withCommonLayout)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Lost Found </h1>
+    </>
+  );
+}
---
+++ src/app/register/page.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+
+const Registerpage = () => {
+    return (
+        <div>
+            <h1>Register Page</h1>
+        </div>
+    );
+};
+
+export default Registerpage;<h1>Register Page</h1>
