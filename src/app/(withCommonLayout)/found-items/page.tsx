8a7d2fa5d0833ace48468d9a7b3e7e290d7467cb
rezwan2230 <rezwan2230@gmail.com>
2025-07-13 23:55:08 +0600

    added post UI component for Found Items page

--- src/app/(withCommonLayout)/found-items/page.tsx
+++ src/app/(withCommonLayout)/found-items/page.tsx
@@ -1,12 +1,16 @@
-/* eslint-disable prettier/prettier */
+import Container from "@/src/components/UI/Container";
+import Post from "@/src/components/UI/Post";
+import axiosInstance from "@/src/lib/AxiosInstance";
+import { IPost } from "@/src/types";
 
+export default async function FoundItems() {
+  const { data } = await axiosInstance.get(`/items`);
 
-const FoundItemsPage = () => {
   return (
-    <div>
-      <h1>Found items Page</h1>
-    </div>
+    <Container>
+      <div className="mx-auto my-3 max-w-[720px]">
+        {data?.data?.map((post: IPost) => <Post key={post?._id} post={post} />)}
+      </div>
+    </Container>
   );
-};
-
-export default FoundItemsPage;
+}
