2b34d533ee8e501f930f1aaa267e2697fc1a0ba9
rezwan2230 <rezwan2230@gmail.com>
2025-07-14 23:00:10 +0600

    create dynamic item details page with server action to fetch item by ID

--- src/services/post/index.ts
+++ src/services/post/index.ts
@@ -1,4 +1,5 @@
-"use server"
+"use server";
+import envConfigue from "@/src/config/envConfigue";
 import axiosInstance from "@/src/lib/AxiosInstance";
 import { revalidateTag } from "next/cache";
 
@@ -14,4 +15,20 @@ export const createPost = async (formData: FormData) => {
   } catch (error) {
     throw new Error("Failed to create post");
   }
+};
+
+
+export const getPost = async (postId: string) => {
+  let fetchOption = {};
+  fetchOption = {
+    cache: "no-store",
+  };
+  const res = await fetch(
+    `${envConfigue.baseApi}/items/${postId}`,
+    fetchOption
+  );
+  if (!res) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
 };
