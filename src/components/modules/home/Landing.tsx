7c9d9dee0efb69bdb336f8a552f2a401d9ec10d9
rezwan2230 <rezwan2230@gmail.com>
2025-07-17 16:00:03 +0600

    added debounce hook for searching

--- src/components/modules/home/Landing.tsx
+++ src/components/modules/home/Landing.tsx
@@ -1,25 +1,37 @@
+"use client";
 import { Input } from "@heroui/input";
 
 import { SearchIcon } from "../../icons";
+import { useForm } from "react-hook-form";
+import useDebounce from "@/src/hooks/debounce.hook";
 
 const Landing = () => {
+  const { register, handleSubmit, watch } = useForm();
+  console.log(useDebounce(watch("search")));
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="h-[calc(100vh-64px)] bg-[url('/lost.jpg')] bg-cover bg-center">
       <div className="pt-32 max-w-xl flex-1 mx-auto">
-        <form className="flex-1">
-          <Input
-            aria-label="Search"
-            classNames={{
-              inputWrapper: "bg-default-100",
-              input: "text-sm",
-            }}
-            placeholder="Search..."
-            size="lg"
-            startContent={
-              <SearchIcon className="pointer-events-none flex-shrink-0 text-base text-default-400" />
-            }
-            type="text"
-          />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex-1">
+            <Input
+              {...register("search")}
+              aria-label="Search"
+              classNames={{
+                inputWrapper: "bg-default-100",
+                input: "text-sm",
+              }}
+              placeholder="Search..."
+              size="lg"
+              startContent={
+                <SearchIcon className="pointer-events-none flex-shrink-0 text-base text-default-400" />
+              }
+              type="text"
+            />
+          </div>
         </form>
       </div>
     </div>
