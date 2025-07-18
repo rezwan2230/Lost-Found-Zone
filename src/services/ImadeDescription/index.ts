c05894cfa02416fb2680ed6da4e3aad315955875
rezwan2230 <rezwan2230@gmail.com>
2025-07-18 18:24:58 +0600

    just trying to implement google generative ai

---
+++ src/services/ImadeDescription/index.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import {GoogleGenAI} from '@google/genai';
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+const ai = new GoogleGenAI({apiKey: GEMINI_API_KEY});
+
+const generateDescription = () => {
+
+    const contents =[
+
+    ];
+
+
+
+
+
+  let responseString = "";
+
+  return responseString;
+};
